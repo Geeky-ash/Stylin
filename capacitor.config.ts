bf7978cfe9dc69ea0944d49dfd3709212f3180e7
Ashirwad <ashirwad.020105@gmail.com>
2026-04-18 14:27:40 +0530

    🚑 Fixed Capacitor Sync Pathing & Ambient Typescript Variables

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stylin.app',
+  appName: 'stylin',
+  webDir: 'dist'
+};
+
+export default config;
